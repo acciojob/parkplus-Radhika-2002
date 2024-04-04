1238777e67212ac24a38700e2d9dbfce8d7c2580
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-04-04 20:02:29 +0530

    Update UserServiceImpl.java

--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -7,33 +7,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository4;
-
     @Override
     public void deleteUser(Integer userId) {
-        userRepository4.deleteById(userId);
+            userRepository4.deleteById(userId);
     }
 
     @Override
     public User updatePassword(Integer userId, String password) {
-        User user = userRepository4.findById(userId).get();
-        user.setPassword(password);
-
-        return userRepository4.save(user);
+        Optional<User> optionalUser = userRepository4.findById(userId);
+        if(optionalUser.isPresent()){
+            User user = optionalUser.get();
+            user.setPassword(password);
+            return userRepository4.save(user);
+        }else{
+            throw new NullPointerException("Invalid User Id");
+        }
     }
 
     @Override
     public void register(String name, String phoneNumber, String password) {
+        //check if user already exist
+
+        //add user
         User user = new User();
         user.setName(name);
-        user.setPhoneNumber(phoneNumber);
         user.setPassword(password);
-        user.setReservationList(new ArrayList<>());
+        user.setPhoneNumber(phoneNumber);
         userRepository4.save(user);
     }
 }
